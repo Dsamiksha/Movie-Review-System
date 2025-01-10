4fe802a08d28dad4771fc43979187a7e9345a485
Dsamiksha <114307278+Dsamiksha@users.noreply.github.com>
2025-01-10 20:47:36 +0530

    Update MovieApplication.java

--- MovieApplication.java
+++ MovieApplication.java
@@ -1,4 +1,4 @@
-package com.goutham.MovieApplication;
+package com.MovieApplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
