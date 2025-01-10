138aa62cfd2a28b9de5d3d6359f587dbf31d9d36
Dsamiksha <114307278+Dsamiksha@users.noreply.github.com>
2025-01-10 20:54:36 +0530

    Update ReviewController.java

--- ReviewController.java
+++ ReviewController.java
@@ -1,7 +1,7 @@
-package com.goutham.MovieApplication.controller;
+package com.MovieApplication.controller;
 
-import com.goutham.MovieApplication.entity.Review;
-import com.goutham.MovieApplication.service.ReviewService;
+import com.MovieApplication.entity.Review;
+import com.MovieApplication.service.ReviewService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
