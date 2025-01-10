543eb249e41e9e026d6294e74b0fb6937897bd53
Dsamiksha <114307278+Dsamiksha@users.noreply.github.com>
2025-01-10 20:58:06 +0530

    Update MovieRepository.java

--- MovieRepository.java
+++ MovieRepository.java
@@ -1,6 +1,6 @@
-package com.goutham.MovieApplication.repository;
+package com.MovieApplication.repository;
 
-import com.goutham.MovieApplication.entity.Movie;
+import com.MovieApplication.entity.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
